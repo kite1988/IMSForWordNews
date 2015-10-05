9e2cf1222cedf4f3e9bbe0589d2ce6a706eb16e7
Kang HJ <kanghongjin@gmail.com>
2015-10-05 13:51:58 +0800

    add db queries to see if it works

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,19 +1,28 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import net.didion.jwnl.JWNLException;
 import play.*;
 import play.libs.Json;
 import play.mvc.*;
+import play.api.db.*;
 
+import sg.edu.nus.comp.nlp.ims.classifiers.CLibLinearEvaluator;
+import sg.edu.nus.comp.nlp.ims.feature.CAllWordsFeatureExtractorCombinationWithSenna;
+import sg.edu.nus.comp.nlp.ims.io.CResultWriter;
+import sg.edu.nus.comp.nlp.ims.util.CJWNL;
+import sg.edu.nus.comp.nlp.ims.util.COpenNLPPOSTagger;
 import views.html.*;
 
 import org.w3c.dom.*;
+
 
 import sg.edu.nus.comp.nlp.ims.implement.CTester;
 import sg.edu.nus.comp.nlp.ims.classifiers.IEvaluator;
 import sg.edu.nus.comp.nlp.ims.io.IResultWriter;
 
+import javax.sql.DataSource;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -21,10 +30,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
 public class Application extends Controller {
+
 
     public Result index() {
         CTester tester = new CTester();
@@ -40,15 +56,33 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready.!!"));
     }
 
-    private boolean isWordInDictionary(String word) {
-        boolean result = false;
+    private boolean isWordInDictionary(String word) throws SQLException {
+        String sql = "SELECT BOOLEAN FROM english_words;";
 
+        Connection conn = play.db.DB.getConnection();
+        try {
+            Statement stmt = conn.createStatement();
+            try {
+                stmt.executeQuery(sql);
+                ResultSet queryRes = stmt.getResultSet();
+                System.out.println(queryRes);
+                System.out.println(queryRes);
+                System.out.println(queryRes);
+                if (queryRes.getBoolean("BOOLEAN")) {
+                    return true;
+                }
 
+            } finally {
+                stmt.close();
+            }
+        } finally {
+            conn.close();
+        }
 
-        return result;
+        return false;
     }
 
-    public Result obtainTranslation() throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, JWNLException, ParserConfigurationException {
+    public Result obtainTranslation() throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, JWNLException, ParserConfigurationException, SQLException {
 
         // extract request params
         final Map<String, String[]> values = request().body().asFormUrlEncoded();
@@ -62,8 +96,21 @@ public class Application extends Controller {
 
 
         // find words to translate
+        List<String> wordsThatCanBeTranslated = new ArrayList<>();
+        String[] tokensInText = text.split(" ");
+        for (String token : tokensInText ) {
+            try {
+                if (isWordInDictionary(token)) {
+                    wordsThatCanBeTranslated.add(token);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw e;
+            }
+        }
 
 
+        result.put("heheheh", wordsThatCanBeTranslated.toString());
 
 
         // write to files expected by ims
@@ -95,13 +142,13 @@ public class Application extends Controller {
         String modelDir = "trainedDir";
         String statDir = "trainedDir";
         String saveDir = "resultDir";
-        String evaluatorName = sg.edu.nus.comp.nlp.ims.classifiers.CLibLinearEvaluator.class.getName();
-        String writerName = sg.edu.nus.comp.nlp.ims.io.CResultWriter.class.getName();
+        String evaluatorName = CLibLinearEvaluator.class.getName();
+        String writerName = CResultWriter.class.getName();
         String lexeltFile = null;
 
         // initial JWordNet
         try {
-            sg.edu.nus.comp.nlp.ims.util.CJWNL.initial(new FileInputStream("lib/prop.xml"));
+            CJWNL.initial(new FileInputStream("lib/prop.xml"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             throw e;
@@ -111,29 +158,29 @@ public class Application extends Controller {
         }
 
 
-        sg.edu.nus.comp.nlp.ims.util.COpenNLPPOSTagger.setDefaultModel("lib/tag.bin.gz");
-        sg.edu.nus.comp.nlp.ims.util.COpenNLPPOSTagger.setDefaultPOSDictionary("lib/tagdict.txt");
+        COpenNLPPOSTagger.setDefaultModel("lib/tag.bin.gz");
+        COpenNLPPOSTagger.setDefaultPOSDictionary("lib/tagdict.txt");
 
 
 
 
-        IEvaluator evaluator = (sg.edu.nus.comp.nlp.ims.classifiers.IEvaluator) Class.forName(evaluatorName)
+        IEvaluator evaluator = (IEvaluator) Class.forName(evaluatorName)
                 .newInstance();
 
         evaluator.setOptions(new String[]{"-m", modelDir, "-s", statDir});
 
 
         // set result writer
-        writerName = sg.edu.nus.comp.nlp.ims.io.CResultWriter.class.getName();
+        writerName = CResultWriter.class.getName();
 
-        IResultWriter writer = (sg.edu.nus.comp.nlp.ims.io.IResultWriter) Class.forName(writerName)
+        IResultWriter writer = (IResultWriter) Class.forName(writerName)
                 .newInstance();
         writer.setOptions(new String[]{"-s", saveDir});
 
         tester.setEvaluator(evaluator);
         tester.setWriter(writer);
 
-        String featureExtractorName = sg.edu.nus.comp.nlp.ims.feature.CAllWordsFeatureExtractorCombinationWithSenna.class.getName();
+        String featureExtractorName = CAllWordsFeatureExtractorCombinationWithSenna.class.getName();
         tester.setFeatureExtractorName(featureExtractorName);
 
 

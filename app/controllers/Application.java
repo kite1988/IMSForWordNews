2ec56c0165c53bbbc85c94f7cb6b36c5ff945e39
Tao Chen <chentaokite@gmail.com>
2016-09-01 13:33:39 +0800

    adapt IMS to accept word and its sentence

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,7 +3,10 @@ package controllers;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.lang3.StringUtils;
 import play.libs.Json;
+import play.libs.Json.*;                        
 import play.mvc.*;
+import play.mvc.BodyParser;                     
+
 
 import play.mvc.Result;
 import sg.edu.nus.comp.nlp.ims.feature.CAllWordsFeatureExtractorCombination;
@@ -24,169 +27,121 @@ import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.zip.GZIPInputStream;
+import java.util.HashMap;
 
 
 public class Application extends Controller {
+    private static HashMap<String, String> idWord;
+    private String chineseDictFile = "dump" + File.separator + "chinese_words.csv";
+	private static String testFlag = "_____";
+
     
     public Result index() {
         return ok(index.render("WordNews!"));
     }
 
-    private boolean isWordInDictionary(String word) throws Exception {
-        try (
-                Connection conn = play.db.DB.getConnection();
-        ) {
-            try (
-                    Statement stmt = conn.createStatement()
-            ) {
-                stmt.executeQuery(
-                        "SELECT id FROM english_words WHERE english_meaning = '" + word + "'"
-                );
-                ResultSet queryRes = stmt.getResultSet();
 
-                return queryRes.next();
-            }
-        }
-    }
+    public Result test() {
+        response().setHeader("Access-Control-Allow-Origin", "*"); 
 
-    private ChinesePronunciationPair getChinesePinyinPairFromId(Long chineseId) throws SQLException {
-        final int offset = 0; // set to non-zero if there are differences between the local db and db on heroku
-
-
-        try (
-                Connection conn = play.db.DB.getConnection()
-        ) {
-            try (
-                    Statement stmt = conn.createStatement()
-            )  {
-                stmt.executeQuery(
-                        "SELECT chinese_meaning, pronunciation FROM chinese_words WHERE id = '" + (chineseId + offset) + "'"
-                );
-                ResultSet results = stmt.getResultSet();
-
-                if (results.next()) {
-                    return new ChinesePronunciationPair(
-                            results.getString("chinese_meaning"),
-                            results.getString("pronunciation")
-                    );
-                }
-            }
-        }
-
-        return ChinesePronunciationPair.NONE;
-    }
-
-    private static class ChinesePronunciationPair {
-        String symbol;
-        String pronunciation;
-
-        public ChinesePronunciationPair(String symbol, String pronunciation) {
-            this.pronunciation = pronunciation;
-            this.symbol = symbol;
-        }
-
-        public static ChinesePronunciationPair NONE = new ChinesePronunciationPair("", "");
+        System.out.println("test");
+        ObjectNode result = Json.newObject();
+        result.put("msg", "ok");
+        result.put("translation", "中文");
+        return ok(result);
     }
 
 
-    public Result obtainTranslation() throws Exception {
+    public Result obtainTranslation() {
+        response().setHeader("Access-Control-Allow-Origin", "*"); 
+
 
         long startTime = System.nanoTime();
         // extract request params
-        final Map<String, String[]> values = request()
-                .body()
-                .asFormUrlEncoded();
-        if( values.get("text") == null || values.get("name") == null || values.get("url") == null || values.get("num_words") == null ) {
+        final Map<String, String[]> values = request().body().asFormUrlEncoded();
+		
+		System.out.println(request().body());
+				
+        if( values==null || values.get("word") == null || values.get("sentence") == null ) {
+             ObjectNode bad_request_result = Json.newObject();
+             bad_request_result.put("msg", "Invalid parameters");
+             //return badRequest(bad_request_result);
+             return ok(bad_request_result);
+         }
+
+        if(values==null) {
             ObjectNode bad_request_result = Json.newObject();
             bad_request_result.put("msg", "Invalid parameters");
-            return badRequest(bad_request_result);
+            return ok(bad_request_result);
         }
-        String textContent = values.get("text")[0];
-        String name = values.get("name")[0];
-        String url = values.get("url")[0];
-        String num_words = values.get("num_words")[0];
-
-        int numWords;
-        try {
-            numWords = Integer.parseInt(num_words);
-        } catch (NumberFormatException | NullPointerException e) {
-            numWords = 2;
-        }
-
+        
+        String word = values.get("word")[0];
+        String sentence = values.get("sentence")[0];
+     
         ObjectNode result = Json.newObject();
-
-        // find words to translate
-        List<String> wordsThatCanBeTranslated = new ArrayList<>();
-        String[] tokensInText = textContent.replaceAll("[^a-zA-Z- ]", " ").split("\\s+");
-
-        for (String token : tokensInText ) {
-            try {
-                if (//!CSurroundingWordFilter.getInstance().filter(token.toLowerCase())
-                       // &&
-                        isWordInDictionary(token.toLowerCase())) {
-                    wordsThatCanBeTranslated.add(token);
-                    if (wordsThatCanBeTranslated.size() >= numWords) {
-                        break;
-                    }
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                throw e;
-            }
+        String translated_text = null;
+        try {
+		    String imsFormat = generateXML(word, sentence);
+            translated_text = getIMSTranslation(imsFormat);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.put("msg", "no-result");
         }
+		
+		if (translated_text!=null) {
+			result.put("msg", "OK");
+			result.put("translation", translated_text);
+		} 
 
-        // write to files expected by ims
-        // xml file
+		long endTime = System.nanoTime();
+		System.out.println("translating " + word + " used " + (endTime-startTime)/1000000 + "ms");
+        return ok(result);
+    }
 
+
+	// TODO: Change the inteface of IMS to accept sentence directly.
+    // write to files expected by ims
+    private String generateXML(String token, String sentence) {
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = null;
         try {
             docBuilder = docFactory.newDocumentBuilder();
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
-            throw e;
+			return null;
         }
+		
         // root elements
         Document doc = docBuilder.newDocument();
         Element rootElement = doc.createElement("corpus");
         doc.appendChild(rootElement);
         rootElement.setAttribute("lang", "english");
 
-        String testFlag =  "_____";
+        Element lexelt = doc.createElement("lexelt");
+        lexelt.setAttribute("item", token);
+        rootElement.appendChild(lexelt);
 
-        for (String token : wordsThatCanBeTranslated) {
-            Element lexelt = doc.createElement("lexelt");
-            lexelt.setAttribute("item", token);
-            rootElement.appendChild(lexelt);
+        Element instance = doc.createElement("instance");
+        lexelt.appendChild(instance);
 
-            Element instance = doc.createElement("instance");
-            lexelt.appendChild(instance);
+        String instanceId = token + ".0";
+        instance.setAttribute("id", instanceId);
+        instance.setAttribute("docsrc", "dummy");
 
-            String instanceId = token + ".0";
-            instance.setAttribute("id", instanceId);
-            instance.setAttribute("docsrc", "dummy");
+        Element answer = doc.createElement("answer");
+        instance.appendChild(answer);
+        answer.setAttribute("instance", instanceId);
+        answer.setAttribute("senseid", "dunno"); // because we are trying to find that out!!!
 
-            Element answer = doc.createElement("answer");
-            instance.appendChild(answer);
-            answer.setAttribute("instance", instanceId);
-            answer.setAttribute("senseid", "dunno"); // because we are trying to find that out!!!
-
-            Element context = doc.createElement("context");
-            instance.appendChild(context);
-            String amendedTextContent = textContent.replaceFirst(token, testFlag + token);
-            context.setTextContent(" " + amendedTextContent + " ");
-        }
-
+        Element context = doc.createElement("context");
+        instance.appendChild(context);
+        context.setTextContent(" " + sentence + " ");
 
         // write to xml
         String xmlFormat = "";
@@ -204,16 +159,12 @@ public class Application extends Controller {
 
         } catch (TransformerException te) {
             System.out.println(te.getMessage());
-            throw te;
+			return null;
         }
 
-
         // write to format expected by ims
-
-
         StringBuilder imsFormat = new StringBuilder();
         for (String lineInFile : xmlFormat.split("\n")) {
-
             if (lineInFile.contains(testFlag)) {
                 StringBuilder updatedLine = new StringBuilder();
 
@@ -227,60 +178,48 @@ public class Application extends Controller {
                     }
                     updatedLine.append(' ');
                 }
-
                 imsFormat.append(updatedLine.toString());
             } else {
                 imsFormat.append(lineInFile);
             }
-
         }
-
-        ObjectNode translated_text = Json.newObject();
-
-        try {
-            CTester senseDisambiguator = getSenseDisambiguator();
-
-            //senseDisambiguator.test(testFileName);
-            senseDisambiguator.testWithXmlString(imsFormat.toString(), null);
-
-            List<Object> results = senseDisambiguator.getResults();
-            for (Object resultObj : results) {
-                CResultInfo imsResult = (CResultInfo)resultObj;
-                for (int instIdx = 0; instIdx < imsResult.size(); instIdx++) {
-                    String docID = imsResult.getDocID(instIdx);
-                    String instanceId = imsResult.getID(instIdx);
-                    String id = imsResult.classes[imsResult.getAnswer(instIdx)];
-
-                    long senseId;
-                    if (StringUtils.isNumeric(id)) {
-                        senseId = Long.parseLong(id);
-                    } else if (id.equals("U")) {
-                        continue;
-                    } else {
-                        throw new Exception("Id is not a number or U :" + id);
-                    }
-                    ChinesePronunciationPair chineseResult = getChinesePinyinPairFromId(senseId);
-
-                    ObjectNode tokenNode =
-                            Json.newObject()
-                                    .put("wordID", senseId)
-                                    .put("chinese", chineseResult.symbol)
-                                    .put("pronunciation", chineseResult.pronunciation)
-                                    .put("isTest", 0);
-
-                    translated_text.set(instanceId.split("\\.")[0], tokenNode);
-                }
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException("Error disambiguating", e);
-        }
-
-        result.put("msg", "OK");
-        result.set("translate_text", translated_text);
-
-        return ok(result);
+		return imsFormat.toString();
     }
+
+
+    private String getIMSTranslation(String imsFormat) {
+		try {
+			CTester senseDisambiguator = getSenseDisambiguator();
+
+			//senseDisambiguator.test(testFileName);
+			senseDisambiguator.testWithXmlString(imsFormat.toString(), null);
+
+			List<Object> results = senseDisambiguator.getResults();
+			if (results.size()==1) {
+				Object resultObj = results.get(0);
+			
+				CResultInfo imsResult = (CResultInfo)resultObj;
+				if (imsResult.size()==1) {
+				
+					String docID = imsResult.getDocID(0);
+					String instanceId = imsResult.getID(0);
+					String senseId = imsResult.classes[imsResult.getAnswer(0)];
+					
+					//System.out.println("sense ID " + senseId);
+
+					if (StringUtils.isNumeric(senseId)) {
+						return getChineseWord(senseId);
+					}
+				}
+			}        
+		}catch (Exception e) {
+			System.err.println("Error disambiguating ");
+			e.printStackTrace();
+		}
+		return null;
+    }
+	
+
 
     private CTester getSenseDisambiguator() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         CTester senseDisambiguator = new CTester();
@@ -294,42 +233,38 @@ public class Application extends Controller {
         return senseDisambiguator;
     }
 
-    private void handleResultsDir(ObjectNode result, File[] filesInDirectory) throws IOException, SQLException {
-        // there should only be one file
-        for (File fileInDirectory : filesInDirectory) {
-            System.out.println(fileInDirectory.getAbsolutePath());
-            if (fileInDirectory.getName().equals("aaa")) {
-                continue; // skip dummy file
-            }
-            BufferedReader bufferedReader = new BufferedReader(
-                    new FileReader(fileInDirectory));
 
-            String lineFromResultFile;
-            while ((lineFromResultFile = bufferedReader.readLine()) != null) {
-
-                String[] tokensInResultsLine = lineFromResultFile.split(" ");
-                long senseId = -1;
-                try {
-                    senseId = Long.parseLong(tokensInResultsLine[2]);
-
-                    ChinesePronunciationPair chineseResult = getChinesePinyinPairFromId(senseId);
-
-                    ObjectNode tokenNode = Json.newObject();
-                    tokenNode.put("wordID", senseId);
-                    tokenNode.put("chinese", chineseResult.symbol);
-                    tokenNode.put("pronunciation", chineseResult.pronunciation);
-                    tokenNode.put("isTest", 0);
-
-                    result.set(tokensInResultsLine[1].split("\\.")[0], tokenNode);
-                } catch (NumberFormatException e) {
-                    // silenced because it is U
-                    assert tokensInResultsLine[2].equals("U");
-                }
-            }
-
-            fileInDirectory.delete();
+    private String getChineseWord(String id) {
+		if (idWord==null) {
+            loadChineseDictionary();
         }
+
+        return idWord.get(id); 
     }
 
+    // Load Chinese word and its ID into memeory.
+    private void loadChineseDictionary() {
+        idWord = new HashMap<String, String>();
+
+        try {
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                new FileInputStream(chineseDictFile), "UTF-8"));
+				
+        String line = null;
+        while ((line=br.readLine())!=null) {
+            line = line.trim();
+            if (!line.isEmpty()) {
+                String[] items = line.split(",");
+                idWord.put(items[0], items[1]);
+				//System.out.println(items[1]);
+            }
+        }
+        br.close();
+		} catch (Exception e) {
+			System.err.println("Error in loading " + chineseDictFile);
+			e.printStackTrace();
+		}
+		System.out.println("size of dict " + idWord.size());
+    }
 
 }
